a0a7fad2882347f827f04d4cc06a33967f751abc
llf <240220492@qq.com>
2022-03-11 21:39:37 +0800

    first version of Chanel

---
+++ include/Chanel.h
@@ -0,0 +1,53 @@
+// Created by llf on 2022/3/11.
+
+#ifndef WEBSERVER_CHANEL_H
+#define WEBSERVER_CHANEL_H
+
+#include<stdint.h>
+#include<functional>
+#include<Other.h>
+#include<EventLoop.h>
+
+class Httpdata;
+
+class Chanel{
+private:
+    int fd;
+    bool is_connect;
+    __uint32_t events;//感兴趣的事件集
+    __uint32_t revents;//已就绪的事件集
+    __uint32_t last_event;//记录上一次的防止重复修改
+
+    Httpdata* holder;
+
+    using CALLBACK=std::function<void()>;
+    CALLBACK read_handle;
+    CALLBACK write_hande;
+    CALLBACK error_handle;
+    CALLBACK disconn_handle;
+
+public:
+    Chanel(int _fd, bool is_conn_, std::chrono::seconds timeout=GlobalValue::client_header_timeout);
+    ~Chanel();
+    void Register_RdHandle(CALLBACK func) {read_handle=std::move(func);}
+    void Register_WrHandle(CALLBACK func) {write_hande=std::move(func);}
+    void Register_ErHandle(CALLBACK func) {error_handle=std::move(func);}
+    void Register_DiscHandle(CALLBACK func) {disconn_handle=std::move(func);}
+    void CallRevents();
+
+    void Set_events(__uint32_t evnt) {events=evnt;}
+    void Set_revents(__uint32_t revnt) {revents=revnt;}
+
+    int Get_fd() {return fd;}
+    Httpdata* Get_holder(){return holder;}
+    bool Get_isconn(){return is_coonect};
+
+private:
+    void CallRdfunc();
+    void CallWrfunc();
+    void CallErfunc();
+    void CallDiscfunc();
+
+};
+
+#endif //WEBSERVER_CHANEL_H
---
+++ src/Chanel.cpp
@@ -0,0 +1,53 @@
+// Created by llf on 2022/3/11.
+
+#include "Chanel.h"
+
+Chanel::Chanel(int _fd, bool is_conn_, std::chrono::seconds timeout): fd(_fd),is_connect(is_conn_)
+{
+}
+
+Chanel::~Chanel()
+{
+    close(fd);
+
+    if(holder)
+    delete holder;
+}
+
+void Chanel::CallRevents()
+{
+    if(this->revents & EPOLLERR)
+    {
+        CallErfunc();
+        return ;
+    }
+    if(this->revents & EPOLLIN)
+        CallRdfunc();
+    if(this->revents & EPOLLOUT)
+        CallWrfunc();
+    if(this->revents & EPOLLRDHUP)
+        CallDiscfunc();
+}
+
+void Chanel::CallRdfunc()
+{
+    if(read_handle) read_handle();
+    else;//打印日志：还没注册
+}
+
+void Chanel::CallWrfunc()
+{
+    if(write_hande) write_hande();
+    else ;//打印日志：还没注册
+}
+void Chanel::CallErfunc()
+{
+    if(error_handle) error_handle();
+    else;//打印日志：还没注册
+}
+
+void Chanel::CallDiscfunc()
+{
+    if(disconn_handle) disconn_handle();
+    else;//打印日志：还没注册
+}
