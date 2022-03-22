15a5775ff65991867e28e204f4267f8bcd166a9d
llf <240220492@qq.com>
2022-03-22 16:54:25 +0800

    完成对首部行的http解析

--- src/HttpData.cpp
+++ src/HttpData.cpp
@@ -121,9 +121,34 @@ sub_state_ParseHTTP HttpData::parse_requestline()
 
 sub_state_ParseHTTP HttpData::parse_header()
 {
-    auto deal_with_perline= [=] ->bool {
+    auto deal_with_perline= [=](std::string str) ->bool {
+        std::regex reg(R"(^(\S*)\:\s(.*)$)");
+        std::smatch res;
+        std::regex_match(str,res,reg);
 
-    };
+        if(!res.empty())
+        {
+            mp[res[1]]=res[2];
+            return true;
+        }
+        else return false;
+
+    };//处理每行数据，将数据的key和value存到map中
+
+    unsigned long current_pos=0;
+    while(true)
+    {
+        current_pos;
+        current_pos=read_buffer.find("\r\n",2);//请求行的\r\n没有删
+        if(current_pos == std::string::npos) return header_data_is_not_complete;//没接收到完整数据
+        if(current_pos ==  2) return header_is_ok;//搜索到空行了
+
+        std::string newline=read_buffer.substr(2,current_pos-2);
+
+        read_buffer=read_buffer.substr(current_pos);//删除已提取的数据，注意：没删除\r\n
+
+        if(!deal_with_perline(newline)) return header_parse_error;//格式出错
+    }
 }
 
 void HttpData::write_and_send(bool error)
--- test/test03-regex.cpp
+++ test/test03-regex.cpp
@@ -7,12 +7,50 @@ using namespace std;
 
 int main()
 {
-    string target="POST /index.html HTTP/1.1";
-    regex reg(R"(^(POST|GET|HEAD|pos|get|head)\s(\S*)\s((HTTP|http)\/\d\.\d)$)");
-    smatch res;
-    regex_match(target,res,reg);
+    auto deal_with_perline= [=](std::string str) ->bool {
+        std::regex reg(R"(^(\S*)\:\s(.*)$)");
+        std::smatch res;
+        std::regex_match(str,res,reg);
+        if(!res.empty())
+        {
+            cout<<res[1]<<"     "<<res[2]<<endl;
+            return true;
+        }
+        else return false;
+    };//处理每行数据，将数据的key和value存到map中
 
-    cout<<res[0]<<endl<<res[1]<<endl<<res[2]<<endl<<res[3]<<endl;
+    unsigned long current_pos=0;
+    string read_buffer=
+            "\r\nHOST: www.XXX.com\r\nUser-Agent: Mozilla/5.0(Windows NT 6.1;rv:15.0) Firefox/15.0\r\n\r\n";
+
+    while(true)
+    {
+        current_pos=read_buffer.find("\r\n",2);//请求行的\r\n没有删
+
+        if(current_pos == std::string::npos)
+        {
+            cout<<"data is not complete"<<endl;
+            return 0;
+        }//没接收到完整数据
+
+        if(current_pos == 2)
+        {
+            cout<<"header is ok"<<endl;
+            return 0;
+        }//搜索到空行了
+
+        std::string newline=read_buffer.substr(2,current_pos-2);
+
+        read_buffer=read_buffer.substr(current_pos);//删除已提取的数据，注意：没删除\r\n
+
+        if(!deal_with_perline(newline))
+        {
+            cout<<"data is wrong"<<endl;
+            return 0;
+        }//格式出错
+
+    }
+
     return 0;
 }
 
